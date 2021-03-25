a81a95690715a89eb82992a302f8faa9081a4f5f
myWsq <wsq961@outlook.com>
2021-03-25 14:09:37 +0800

    style: 标点符号修正

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -8,17 +8,17 @@
       <strong>2015 - 2019</strong>
     </SpaceBetween>
     <ul>
-      <li>北邮蓝图工作室创始人</li>
-      <li>优秀毕业设计</li>
+      <li>北邮蓝图工作室创始人；</li>
+      <li>优秀毕业设计。</li>
     </ul>
     <SpaceBetween class="mt-3">
       <strong>小米 - 软件与体验部 - 系统安全部 - 前端平台</strong>
       <strong>2019.7 至今</strong>
     </SpaceBetween>
     <ul>
-      <li>2019 年度绩效 S、2020 年中绩效 S、2020 年度绩效 A</li>
-      <li>小米 A 计划成员、Top 应届生</li>
-      <li>MIUI 学院讲师</li>
+      <li>2019 年度绩效 S、2020 年中绩效 S、2020 年度绩效 A；</li>
+      <li>小米 A 计划成员、Top 应届生；</li>
+      <li>MIUI 学院讲师。</li>
     </ul>
   </div>
 </template>
