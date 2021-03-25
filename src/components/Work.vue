8ec27d6b43e14272a421aad227d680513c1a10d9
myWsq <wsq961@outlook.com>
2021-03-25 14:08:36 +0800

    feat: 添加详细工作经历和教育经历

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -2,14 +2,23 @@
   <div>
     <h2>教育/工作经历</h2>
     <SpaceBetween>
-      <span
-        >北京邮电大学/北京联合大学 (双培计划) - 计算机科学与技术 - 本科</span
-      >
+      <strong>
+        北京邮电大学/北京联合大学 (双培计划) - 计算机科学与技术 - 本科
+      </strong>
       <strong>2015 - 2019</strong>
     </SpaceBetween>
-    <SpaceBetween class="mt-1">
-      <span>小米 - 软件与体验部 - 系统安全部 - 前端平台</span>
+    <ul>
+      <li>北邮蓝图工作室创始人</li>
+      <li>优秀毕业设计</li>
+    </ul>
+    <SpaceBetween class="mt-3">
+      <strong>小米 - 软件与体验部 - 系统安全部 - 前端平台</strong>
       <strong>2019.7 至今</strong>
     </SpaceBetween>
+    <ul>
+      <li>2019 年度绩效 S、2020 年中绩效 S、2020 年度绩效 A</li>
+      <li>小米 A 计划成员、Top 应届生</li>
+      <li>MIUI 学院讲师</li>
+    </ul>
   </div>
 </template>
