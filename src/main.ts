395a139d4d2c61363ad356c8791d28d878aca101
myWsq <wsq961@outlook.com>
2021-03-25 12:39:40 +0800

    chore: 添加 ssg

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import "virtual:windi.css";
-import { createApp } from "vue";
+import { ViteSSG } from "vite-ssg/single-page";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+export const createApp = ViteSSG(App);
